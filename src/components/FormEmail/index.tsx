3935d12b9abd45a79270ccd8954cc2b36e557873
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-29 14:42:42 +0530

    Feature: Add Email Form Component

---
+++ src/components/FormEmail/index.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { useAppSelector } from "../../app/hooks";
+import { validate } from "../../app/utils/validate";
+import { selectStatus } from "../../features/loading";
+import { selectVerifySent } from "../../features/verifyEmailSent";
+import { CustomButton } from "../Button";
+import { CustomInput } from "../CustomInput";
+import { LabelledFormComponent } from "../LabelledFormComponent";
+import styles from "./style.module.css";
+
+export interface FormEmailProps {
+	resendEmailText: string;
+	onChangePage: Function;
+	onSubmit: Function;
+	changePageText: string;
+}
+
+export const FormEmail: React.FC<FormEmailProps> = ({
+	resendEmailText,
+	onChangePage,
+	onSubmit,
+	changePageText,
+}) => {
+	const [email, setEmail] = useState("");
+	const [emailErr, setEmailErr] = useState("");
+
+	const status = useAppSelector(selectStatus);
+	const verifyEmail = useAppSelector(selectVerifySent);
+
+	function handleChange(val: string) {
+		const valid = validate({ val, type: "email" });
+		setEmailErr(valid);
+		setEmail(val);
+	}
+
+	return (
+		<>
+			<LabelledFormComponent labelText="Email">
+				{emailErr !== "" && <div className={styles.err}>{emailErr}</div>}
+				<CustomInput
+					type="email"
+					containerStyle={{ margin: "5px 0px" }}
+					backgroundColor="#ededed"
+					placeholder="Email"
+					className={styles.email}
+					name="email"
+					value={email}
+					onChange={(e) => handleChange(e.target.value)}
+				/>
+			</LabelledFormComponent>
+			{verifyEmail && (
+				<CustomButton
+					isSecondary
+					text={resendEmailText}
+					onClick={() => onSubmit(email)}
+				/>
+			)}
+			<CustomButton
+				className={styles.button}
+				type="primary"
+				text="Confirm Email"
+				loading={status === "processing"}
+				onClick={() => onSubmit(email)}
+			/>
+			<div className={styles.changeView}>
+				{changePageText}
+				<CustomButton
+					isSecondary={true}
+					size="small"
+					text="Login"
+					onClick={() => onChangePage()}
+				/>
+			</div>
+		</>
+	);
+};
