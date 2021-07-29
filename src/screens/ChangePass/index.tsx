ce780a09e195cc4a954b5986b72e13381809246a
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-29 14:45:30 +0530

    Feature: Add Change Password Screen

---
+++ src/screens/ChangePass/index.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { validate } from "../../app/utils/validate";
+import { CustomInput } from "../../components/CustomInput";
+import { FormContainer } from "../../components/FormContainer";
+import { LabelledFormComponent } from "../../components/LabelledFormComponent";
+import { SetPass } from "../../components/SetPass";
+import { authorizationInit } from "../../features/authorization";
+import { changePassInit, resetPassInit } from "../../features/forgotPAss";
+import { selectStatus } from "../../features/loading";
+import styles from "./style.module.css";
+
+export interface ChangePassProps {
+	forgot?: boolean;
+}
+
+export const ChangePass: React.FC<ChangePassProps> = ({ forgot = false }) => {
+	const [pass, setPass] = useState("");
+	const [passErr, setPassErr] = useState("");
+	const [once, setOnce] = useState(false);
+	const [submitted, setSubmitted] = useState(false);
+
+	const status = useAppSelector(selectStatus);
+	const dispatch = useAppDispatch();
+
+	const history = useHistory();
+
+	const location = useLocation();
+	const token = location.search.split("=")[1];
+
+	function handleChange(val: string) {
+		const validP = validate({ val, type: "pass" });
+		setPassErr(validP);
+		setPass(val);
+	}
+
+	function handleSubmit(newPass: string) {
+		console.log(newPass);
+		if (forgot) {
+			dispatch(resetPassInit(token, newPass));
+		} else {
+			dispatch(changePassInit(pass, newPass));
+		}
+		setSubmitted(true);
+	}
+
+	useEffect(() => {
+		if (!once) {
+			dispatch(authorizationInit("/changePassword"));
+			setOnce(true);
+		}
+		if (status === "passed" && once && forgot) {
+			history.push("/home/loggedin");
+		} else if (status === "failed" && once && !forgot) {
+			history.push("/login");
+		}
+		if (submitted) {
+			history.push("/login");
+		}
+	}, [status]);
+
+	return (
+		<FormContainer pageTitle="CHANGE PASSWORD">
+			{!forgot && (
+				<LabelledFormComponent
+					labelText="Current Password"
+					error
+					errorText={passErr}
+				>
+					<CustomInput
+						backgroundColor="#ededed"
+						containerStyle={{ margin: "5px 0px" }}
+						placeholder="Confirm Password"
+						type="password"
+						className={styles.passwordInput}
+						name="confirm-password"
+						value={pass}
+						onChange={(e) => handleChange(e.target.value)}
+					/>
+				</LabelledFormComponent>
+			)}
+			<SetPass onSubmit={handleSubmit} />
+		</FormContainer>
+	);
+};
