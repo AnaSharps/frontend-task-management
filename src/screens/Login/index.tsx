df2a54dc7b5685013e0495c96003d163b67569a3
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-07-29 14:46:36 +0530

    Feature: Separate Login and Registration screens

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -6,17 +6,14 @@ import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { CustomButton } from "../../components/Button";
 import { LabelledFormComponent } from "../../components/LabelledFormComponent";
 import { changeSignup, selectSignup } from "../../features/signupForm";
-import { sendVerify, selectVerifySent } from "../../features/verifyEmailSent";
-import { changeLoading } from "../../features/loading";
-import { useHistory, useLocation } from "react-router-dom";
-import axios from "axios";
-import { host } from "../../app/constants";
+import { changeLoading, selectStatus } from "../../features/loading";
+import { useHistory } from "react-router-dom";
 import { Spin } from "antd";
-import { selectToken, setToken } from "../../features/token";
-import { selectIsAdmin } from "../../features/isAdmin";
 import { loginInit } from "../../features/login";
 import { registrationInit } from "../../features/register";
 import { authorizationInit } from "../../features/authorization";
+import { validate } from "../../app/utils/validate";
+import { FormContainer } from "../../components/FormContainer";
 
 // axios.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
 
@@ -24,123 +21,100 @@ export interface LoginProps {}
 
 export const Login: React.FC<LoginProps> = () => {
 	const signup = useAppSelector(selectSignup);
-	const verifyEmail = useAppSelector(selectVerifySent);
-	// const loading = useAppSelector(selectLoading);
-	// const token = useAppSelector(selectToken);
-	const isAdmin = useAppSelector(selectIsAdmin);
+	const status = useAppSelector(selectStatus);
 	const dispatch = useAppDispatch();
 
 	const [email, setEmail] = useState("");
 	const [emailErr, setEmailErr] = useState("");
-	const [passErr, setPassErr] = useState("");
 	const [pass, setPass] = useState("");
+	const [once, setOnce] = useState(false);
 
 	let history = useHistory();
-	let location = useLocation();
+
+	const [display, setDisplay] = useState(false);
 
 	useEffect(() => {
-		dispatch(authorizationInit(location.pathname));
-	}, []);
+		if (!once) {
+			setOnce(true);
+			// dispatch(authorizationInit("/admin/loggedin", "/home/loggedin"));
+			dispatch(authorizationInit("/home/loggedin"));
+		} else if (status === "passed") {
+			history.push("/home/loggedin");
+		} else if (status === "failed") {
+			setDisplay(true);
+		}
+	}, [status]);
 
 	function handleChange(val: string, type: "email" | "pass") {
 		if (type === "email") {
-			// const valid = validate(val, "email");
-			// setEmailErr(valid);
+			const valid = validate({ val, type: "email" });
+			setEmailErr(valid);
 			setEmail(val);
 		} else {
-			// const valid = validate(val, "pass");
-			// setPassErr(valid);
 			setPass(val);
 		}
 	}
 
-	function handleSubmit(authType: "login" | "register") {
-		if (authType === "login") {
-			console.log("starting login");
-			dispatch(loginInit({ email, pass }));
-			console.log("completed login");
-		} else {
-			dispatch(registrationInit(email));
-		}
+	function handleSubmit() {
+		dispatch(loginInit({ email, pass }));
 	}
 
 	return (
-		<div className={styles.mainContainer}>
-			{/* {loading ? (
-				<Spin />
-			) : (
-				<> */}
-			<span className={styles.header}>{signup ? "REGISTER" : "LOGIN"}</span>
-			<div className={styles.container}>
-				<form className={styles.formWrapper}>
-					<div className={styles.formContainer}>
-						<LabelledFormComponent labelText="Email">
-							{emailErr !== "" && <div className={styles.err}>{emailErr}</div>}
-							<CustomInput
-								type="email"
-								containerStyle={{ margin: "5px 0px" }}
-								backgroundColor="#ededed"
-								placeholder="Email"
-								className={styles.email}
-								name="email"
-								value={email}
-								onChange={(e) => handleChange(e.target.value, "email")}
-							/>
-						</LabelledFormComponent>
-						{!signup && (
-							<LabelledFormComponent labelText="Password">
-								{passErr !== "" && <div className={styles.err}>{passErr}</div>}
-								<CustomInput
-									backgroundColor="#ededed"
-									containerStyle={{ margin: "5px 0px" }}
-									placeholder="Password"
-									type="password"
-									className={styles.passwordInput}
-									name="password"
-									value={pass}
-									onChange={(e) => handleChange(e.target.value, "pass")}
-								/>
-							</LabelledFormComponent>
-						)}
-						{!signup && (
-							<CustomButton
-								isSecondary={true}
-								size="small"
-								text="Forgot Password?"
-								onClick={() => console.log("Forgot Password")}
-							/>
-						)}
-						{signup && verifyEmail && (
-							<CustomButton
-								isSecondary={true}
-								size="small"
-								text="Resend Verification Link"
-								onClick={() => console.log("Forgot Password")}
-							/>
-						)}
-						<CustomButton
-							className={styles.button}
-							type="primary"
-							text={signup ? "Confirm Email" : "Login"}
-							loading={false}
-							onClick={() =>
-								signup ? handleSubmit("register") : handleSubmit("login")
-							}
+		<>
+			{display && (
+				<FormContainer pageTitle="LOGIN">
+					<LabelledFormComponent
+						labelText="Email"
+						error={emailErr !== ""}
+						errorText={emailErr}
+					>
+						<CustomInput
+							type="email"
+							containerStyle={{ margin: "5px 0px" }}
+							backgroundColor="#ededed"
+							placeholder="Email"
+							className={styles.email}
+							name="email"
+							value={email}
+							onChange={(e) => handleChange(e.target.value, "email")}
 						/>
-						<div className={styles.changeView}>
-							{signup ? "Already a user?" : "New to Task Manager?"}
-							<CustomButton
-								isSecondary={true}
-								size="small"
-								text={signup ? "Login" : "Register"}
-								onClick={() => dispatch(changeSignup())}
-							/>
-						</div>
+					</LabelledFormComponent>
+					<LabelledFormComponent labelText="Password">
+						<CustomInput
+							backgroundColor="#ededed"
+							containerStyle={{ margin: "5px 0px" }}
+							placeholder="Password"
+							type="password"
+							className={styles.passwordInput}
+							name="password"
+							value={pass}
+							onChange={(e) => handleChange(e.target.value, "pass")}
+						/>
+					</LabelledFormComponent>
+					<CustomButton
+						isSecondary={true}
+						size="small"
+						text="Forgot Password?"
+						onClick={() => history.push("/forgotPassword")}
+					/>
+					<CustomButton
+						className={styles.button}
+						type="primary"
+						text={signup ? "Confirm Email" : "Login"}
+						loading={false}
+						onClick={handleSubmit}
+					/>
+					<div className={styles.changeView}>
+						New to Task Manager?
+						<CustomButton
+							isSecondary={true}
+							size="small"
+							text="Register"
+							onClick={() => history.push("/register")}
+						/>
 					</div>
-				</form>
-			</div>
-			{/* </>
-			)} */}
-		</div>
+				</FormContainer>
+			)}
+		</>
 	);
 };
