c3efede9d1f3391b3247e27e835bc6d57e81ee97
Prashansa Dadu <65507065+AnaSharps@users.noreply.github.com>
2021-08-05 12:01:18 +0530

    Fix routing bug

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,6 +5,7 @@ import { Redirect, BrowserRouter as Router, Switch } from "react-router-dom";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { authorizationInit } from "../../features/authorization";
 import { selectStatus } from "../../features/loading";
+import { selectLoginChanged } from "../../features/login";
 import styles from "./style.module.css";
 
 export interface HomeProps {
@@ -16,18 +17,19 @@ export const Home: React.FC<HomeProps> = ({ admin = false, ...props }) => {
 	const location = useLocation();
 	const dispatch = useAppDispatch();
 	const status = useAppSelector(selectStatus);
+	const loginchanged = useAppSelector(selectLoginChanged);
 	const [once, setOnce] = useState(true);
 
 	useEffect(() => {
-		if (once) {
+		if (once || loginchanged) {
 			dispatch(authorizationInit(location.pathname));
 			setOnce(false);
 		}
-	}, [once]);
+	}, [once, loginchanged]);
 
 	return (
 		<>
-			{once && status === "failed"
+			{(once || loginchanged) && status === "failed"
 				? history.push("/app/login")
 				: props.children}
 		</>
